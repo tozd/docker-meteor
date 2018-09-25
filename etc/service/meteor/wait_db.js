925201938309e968e7909356d4137bd7a87da32b
Andi <h4de5@users.noreply.github.com>
2018-09-25 15:30:10 +0200

    Update wait_db.js

--- etc/service/meteor/wait_db.js
+++ etc/service/meteor/wait_db.js
@@ -1,33 +1,33 @@
 var mongodb = require('mongodb');
 var waitingFor = 2;
 function tryConnect(url) {
-	mongodb.MongoClient.connect(url, {
-		connectTimeoutMS: 5000,
-		socketTimeoutMS: 5000
-	}, function (error, db) {
-		if (error === null) {
-			const localDb = db.db('local');
-			const admin = localDb.admin();
+  mongodb.MongoClient.connect(url, {
+    connectTimeoutMS: 5000,
+    socketTimeoutMS: 5000
+  }, function (error, db) {
+    if (error === null) {
+      const localDb = db.db('local');
+      const admin = localDb.admin();
 
-			// Ping the server
-			admin.ping(function (err, result) {
-				if (error === null) {
-					if (--waitingFor <= 0) {
-						process.exit(0);
-					}
-					return;
-				}
-				else {
-					console.error("Waiting for database", error);
-				}
-				setTimeout(function () { tryConnect(url) }, 100);
-			});
-			return;
-		}
-		else {
-			console.error("Waiting for database", error);
-		}
-		setTimeout(function () { tryConnect(url) }, 100);
-	});
+      // Ping the server
+      admin.ping(function (err, result) {
+        if (error === null) {
+          if (--waitingFor <= 0) {
+            process.exit(0);
+          }
+          return;
+        }
+        else {
+          console.error("Waiting for database", error);
+        }
+        setTimeout(function () { tryConnect(url) }, 100);
+      });
+      return;
+    }
+    else {
+      console.error("Waiting for database", error);
+    }
+    setTimeout(function () { tryConnect(url) }, 100);
+  });
 }
 tryConnect(process.env.MONGO_URL);
