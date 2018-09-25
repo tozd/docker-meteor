64f17fc36f0089950b9845923b224e04320ca08f
Andi <h4de5@users.noreply.github.com>
2018-09-25 15:28:56 +0200

    ping method moved to Admin property in mongo v3
    See: http://mongodb.github.io/node-mongodb-native/3.0/api/Admin.html#~resultCallback
    also options in connect parameters moved to top level.

--- etc/service/meteor/wait_db.js
+++ etc/service/meteor/wait_db.js
@@ -1,40 +1,33 @@
 var mongodb = require('mongodb');
-
 var waitingFor = 2;
-
 function tryConnect(url) {
   mongodb.MongoClient.connect(url, {
-      server: {
-        socketOptions: {
-          connectTimeoutMS: 5000,
-          socketTimeoutMS: 5000
-        }
-      }
+			connectTimeoutMS: 5000,
+			socketTimeoutMS: 5000
     }, function (error, db) {
-    if (error === null) {
-      db.command({ping: 1}, function(error, result) {
-        if (error === null) {
-          if (--waitingFor <= 0) {
-            process.exit(0);
-          }
-          return;
-        }
-        else {
-          console.error("Waiting for database", error);
-        }
+			if (error === null) {
+				const localDb = db.db('local');
+				const admin = localDb.admin();
 
-        setTimeout(function() { tryConnect(url) }, 100);
-      });
+				// Ping the server
+				admin.ping(function (err, result) {
+					if (error === null) {
+						if (--waitingFor <= 0) {
+							process.exit(0);
+						}
+						return;
+					}
+					else {
+						console.error("Waiting for database", error);
+					}
+					setTimeout(function () { tryConnect(url) }, 100);
+				});
       return;
     }
     else {
       console.error("Waiting for database", error);
     }
-
     setTimeout(function() { tryConnect(url) }, 100);
   });
 }
-
 tryConnect(process.env.MONGO_URL);
-tryConnect(process.env.MONGO_OPLOG_URL);
-
