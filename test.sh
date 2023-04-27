466e52144923350d46d92381f25a1e0879b288be
Mitar <mitar.git@tnode.com>
2023-04-27 22:54:52 +0200

    Iterating on tests.

--- test.sh
+++ test.sh
@@ -15,16 +15,21 @@ fi
 
 echo "Preparing"
 apk add --no-cache git || exit 3
-git clone https://github.com/meteor/clock test || exit 4
 
+echo "Creating test app"
+git clone https://github.com/meteor/clock test || exit 4
+# Checkout app at Meteor release 0.9.2, the earliest version we support.
+git -C test checkout ed95003ef71c5e0b5dbfd4054af67608b3b7a412
 cd test
 echo "FROM ${CI_REGISTRY_IMAGE}:${TAG}" > Dockerfile
+# We update to the version we are testing.
+docker run --rm --entrypoint '' --volume "$(pwd):/app" --workdir /app "${CI_REGISTRY_IMAGE}:${TAG}" meteor --release "$METEOR_VERSION" update --all-packages || exit 5
 
 echo "Building Docker image"
-docker build -t testimage -f Dockerfile . || exit 5
+docker build -t testimage -f Dockerfile . || exit 6
 
 echo "Running Docker image"
-docker run -d --name test --rm -p 3000:3000 testimage || exit 6
+docker run -d --name test --rm -p 3000:3000 testimage || exit 7
 
 echo "Sleeping"
 sleep 10
@@ -34,6 +39,6 @@ wget -q -O - http://docker:3000 | grep -q '<title>SVG Clock Demo</title>'
 result=$?
 
 echo "Stopping Docker image"
-docker stop test || exit 7
+docker stop test || exit 8
 
 exit "$result"
