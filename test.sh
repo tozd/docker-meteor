200bd2a54cf983a54526799c0475c806011efeca
Mitar <mitar.git@tnode.com>
2023-04-27 22:17:42 +0200

    Test building Meteor app.

--- test.sh
+++ test.sh
@@ -13,4 +13,27 @@ if [ "$installed_version" != "Meteor ${METEOR_VERSION}" ]; then
   exit 2
 fi
 
-exit 0
+docker run --name test --entrypoint '' "${CI_REGISTRY_IMAGE}:${TAG}" meteor create --example clock /tmp/test || exit 3
+docker cp test:/tmp/test test || exit 4
+docker stop test || exit 5
+
+cd test
+echo "FROM ${CI_REGISTRY_IMAGE}:${TAG}" > Dockerfile
+
+echo "Building Docker image"
+docker build -t testimage -f Dockerfile .
+
+echo "Running Docker image"
+docker run -d --name test --rm -p 3000:3000 testimage || exit 6
+
+echo "Sleeping"
+sleep 10
+
+echo "Testing"
+wget -q -O - http://docker:3000 | grep -q '<title>SVG Clock Demo</title>'
+result=$?
+
+echo "Stopping Docker image"
+docker stop test || exit 7
+
+exit "$result"
