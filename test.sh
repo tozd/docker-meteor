409799e956af7e18542281124bdcd10590a422a3
Mitar <mitar.git@tnode.com>
2023-05-01 02:41:03 +0200

    Update test script.

--- test.sh
+++ test.sh
@@ -1,44 +1,123 @@
 #!/bin/sh
 
+set -e
+
+version() {
+  echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'
+}
+
+cleanup_docker=0
+cleanup_mongo=0
+cleanup_app=0
+cleanup_image=0
+cleanup_config=0
+cleanup() {
+  if [ "$cleanup_docker" -ne 0 ]; then
+    echo "Stopping Docker image"
+    docker stop test
+  fi
+
+  if [ "$cleanup_mongo" -ne 0 ]; then
+    echo "Stopping MongoDB"
+    docker stop mongotest
+  fi
+
+  if [ "$cleanup_app" -ne 0 ]; then
+    echo "Removing test app"
+    rm -rf test
+  fi
+
+  if [ "$cleanup_image" -ne 0 ]; then
+    echo "Removing Docker image"
+    docker image rm testimage
+  fi
+
+  if [ "$cleanup_config" -ne 0 ]; then
+    rm -f run.config
+  fi
+}
+
+trap cleanup EXIT
+
 METEOR_VERSION="${TAG##*-}"
 
 installed_version="$(docker run --rm --entrypoint '' "${CI_REGISTRY_IMAGE}:${TAG}" meteor --version | tail -n 1)"
-if [ $? -ne 0 ]; then
-  echo "Error: Getting Meteor version failed"
+if [ "$installed_version" != "Meteor ${METEOR_VERSION}" ]; then
+  echo "Error: $installed_version is installed, wanted ${METEOR_VERSION}"
   exit 1
 fi
 
-if [ "$installed_version" != "Meteor ${METEOR_VERSION}" ]; then
-  echo "Error: $installed_version is installed, wanted ${METEOR_VERSION}"
-  exit 2
-fi
-
 echo "Preparing"
-apk add --no-cache git || exit 3
+apk add --no-cache git
 
 echo "Creating test app"
-git clone https://github.com/meteor/clock test || exit 4
-# Checkout app at Meteor release 0.9.2, the earliest version we support.
-git -C test checkout ed95003ef71c5e0b5dbfd4054af67608b3b7a412
-cd test
-echo "FROM ${CI_REGISTRY_IMAGE}:${TAG}" > Dockerfile
-# We update to the version we are testing.
-docker run --rm --entrypoint '' --volume "$(pwd):/app" --workdir /app "${CI_REGISTRY_IMAGE}:${TAG}" meteor --release "$METEOR_VERSION" update --all-packages || exit 5
+git clone https://github.com/meteor/clock test
+cleanup_app=1
+
+if [ "$METEOR_VERSION" = "1.0.2" ]; then
+  target="1.0.1"
+elif [ "$METEOR_VERSION" = "1.1.0.1" ]; then
+  target="1.1"
+else
+  target="$METEOR_VERSION"
+fi
+
+# Find a commit of the app which is closest to the version we want. We really
+# want to start at 0.9.2, the earliest version we support, but there is
+# messy history (conflicts) so we start around 0.9.0.1 instead.
+found=0
+for commit in $(git -C test rev-list HEAD ^03d3b986dfc355901eae9a2740375d5fd14e7f6f) ; do
+  git -C test checkout --quiet "$commit"
+  release="$(cat test/.meteor/release)"
+  if [ "$release" = "METEOR@$target" ]; then
+    found=1
+    break
+  fi
+done
+if [ $found -eq 0 ]; then
+  # We could not find the exact version, so we go with the latest one and update it to the version we are testing.
+  echo "Updating test app"
+  git -C test checkout --quiet master
+  echo "{}" > "test/package.json"
+  docker run --rm --entrypoint '' --volume "$(pwd)/test:/app" --workdir /app --env NODE_TLS_REJECT_UNAUTHORIZED=0 "${CI_REGISTRY_IMAGE}:${TAG}" meteor update --release "$METEOR_VERSION"
+  if docker run --rm --entrypoint '' --volume "$(pwd)/test:/app" --workdir /app --env NODE_TLS_REJECT_UNAUTHORIZED=0 "${CI_REGISTRY_IMAGE}:${TAG}" meteor npm version > /dev/null ; then
+    if [ "$(version $METEOR_VERSION)" -ge "$(version "1.8.2")" ]; then
+      docker run --rm --entrypoint '' --volume "$(pwd)/test:/app" --workdir /app --env NODE_TLS_REJECT_UNAUTHORIZED=0 "${CI_REGISTRY_IMAGE}:${TAG}" meteor npm install --save @babel/runtime
+    else
+      docker run --rm --entrypoint '' --volume "$(pwd)/test:/app" --workdir /app --env NODE_TLS_REJECT_UNAUTHORIZED=0 "${CI_REGISTRY_IMAGE}:${TAG}" meteor npm install --save babel-runtime
+      docker run --rm --entrypoint '' --volume "$(pwd)/test:/app" --workdir /app --env NODE_TLS_REJECT_UNAUTHORIZED=0 "${CI_REGISTRY_IMAGE}:${TAG}" meteor npm install --save @babel/runtime@7.0.0-beta.55
+    fi
+  fi
+elif [ "$target" != "$METEOR_VERSION" ]; then
+  # We could not find the exact version, so we use an older version and update it to the version we are testing.
+  echo "Updating test app"
+  docker run --rm --entrypoint '' --volume "$(pwd)/test:/app" --workdir /app --env NODE_TLS_REJECT_UNAUTHORIZED=0 "${CI_REGISTRY_IMAGE}:${TAG}" meteor update --release "$METEOR_VERSION"
+fi
 
 echo "Building Docker image"
-docker build -t testimage -f Dockerfile . || exit 6
+echo "FROM ${CI_REGISTRY_IMAGE}:${TAG}" > test/Dockerfile
+docker build -t testimage -f test/Dockerfile test
+cleanup_image=1
+
+echo "MONGODB_ADMIN_PWD='test'" > run.config
+echo "MONGODB_CREATE_PWD='test'" >> run.config
+echo "MONGODB_OPLOGGER_PWD='test'" >> run.config
+echo 'export MONGO_URL="mongodb://meteor:${MONGODB_CREATE_PWD}@mongotest/meteor"' >> run.config
+echo 'export MONGO_OPLOG_URL="mongodb://oplogger:${MONGODB_OPLOGGER_PWD}@mongotest/local?authSource=admin"
+' >> run.config
+cleanup_config=1
+
+echo "Running MongoDB"
+docker run -d --name mongotest --rm -p 27017:27017 -v "$(pwd)/run.config:/etc/service/mongod/run.config" registry.gitlab.com/tozd/docker/meteor-mongodb:2.6
+cleanup_mongo=1
 
 echo "Running Docker image"
-docker run -d --name test --rm -p 3000:3000 testimage || exit 7
+docker run -d --name test --rm -p 3000:3000 -v "$(pwd)/run.config:/etc/service/meteor/run.config" --link mongotest:mongotest testimage
+cleanup_docker=1
 
+# It is OK to sleep just for 10 seconds because Meteor Docker image knows how to wait for MongoDB to become ready.
 echo "Sleeping"
 sleep 10
 
 echo "Testing"
-wget -q -O - http://docker:3000 | grep -q '<title>SVG Clock Demo</title>'
-result=$?
-
-echo "Stopping Docker image"
-docker stop test || exit 8
-
-exit "$result"
+wget -T 30 -q -O - http://docker:3000 | grep -q '<title>SVG Clock Demo</title>'
